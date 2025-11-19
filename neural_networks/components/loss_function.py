a67e409f00f0a5c294cfd8937ddc7ed0f0398cd6
Sophia Shen <sophiashen767@outlook.com>
2025-11-18 21:00:55 -0500

    final loss function

--- neural_networks/components/loss_function.py
+++ neural_networks/components/loss_function.py
@@ -1,18 +1,13 @@
-# builtin
-
 # external
 import numpy as np
 
-# internal
-
-
-# superclass / interface for loss functions
 class LossFunction:
-    # returns the base gradient of whatever loss function you're using
+    """Superclass / interface for loss function"""
+    # returns the base gradient
     def get_training_loss(self, response: np.ndarray, prediction: np.ndarray) -> np.ndarray:
         raise NotImplementedError
 
-    # returns a scalar evaluation of the loss (e.g. mean cross-entropy)
+    # returns a scalar evaluation of the loss (mean cross-entropy)
     def get_test_loss(self, response: np.ndarray, prediction: np.ndarray) -> float:
         raise NotImplementedError
     
@@ -29,9 +24,8 @@ class CrossEntropy(LossFunction):
         return grad
     
     def get_test_loss(self, response: np.ndarray, prediction: np.ndarray) -> float:
-        """Mean cross-entropy value (no gradient)."""
         pred = np.atleast_2d(prediction)
         N = pred.shape[0]
-        py = pred[np.arange(N), response]
-        log_likelihood = -np.log(np.clip(py, 1e-12, 1 - 1e-12))
+        py = pred[np.arange(N), response]                #selects only probability value assigned to correct class
+        log_likelihood = -np.log(np.clip(py, 1e-12, 1 - 1e-12))     #ensures probability is never 0 or 1
         return float(np.mean(log_likelihood))
